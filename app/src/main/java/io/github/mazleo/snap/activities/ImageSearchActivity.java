56034611f7de5fa07228000e1648222c5cfca724
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-28 12:52:41 -0400

    Add ImageSearchActivity

---
+++ app/src/main/java/io/github/mazleo/snap/activities/ImageSearchActivity.java
@@ -0,0 +1,16 @@
+package io.github.mazleo.snap.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.mazleo.snap.R;
+
+public class ImageSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_search);
+    }
+}
