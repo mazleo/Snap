8b97691628515d2502958f0d67dea60447d2a07b
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-30 14:11:06 -0400

    Create SearchInfo class

---
+++ app/src/main/java/io/github/mazleo/snap/util/SearchInfo.java
@@ -0,0 +1,9 @@
+package io.github.mazleo.snap.util;
+
+public class SearchInfo {
+    public static final int RESULTS_PER_PAGE = 30;
+    public static final int SEARCH_TYPE_IMAGE = 1;
+    public static final int SEARCH_TYPE_VIDEO = 2;
+    public static final int SEARCH_NO_PROGRESS = 0;
+    public static final int SEARCH_IN_PROGRESS = 1;
+}
