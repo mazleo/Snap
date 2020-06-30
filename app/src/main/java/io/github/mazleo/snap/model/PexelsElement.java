68268a7b3f922e7492b66ec2801aa6364c665f68
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-30 14:57:45 -0400

    Implement PexelsElement class

---
+++ app/src/main/java/io/github/mazleo/snap/model/PexelsElement.java
@@ -0,0 +1,37 @@
+package io.github.mazleo.snap.model;
+
+public class PexelsElement {
+    private int id;
+    private int width;
+    private int height;
+
+    public PexelsElement(int id, int width, int height) {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
