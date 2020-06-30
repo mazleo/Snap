d4bf50a966b2a22888de4ebbec1e67f98a66df53
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-30 15:08:02 -0400

    Implement PexelsImage class

---
+++ app/src/main/java/io/github/mazleo/snap/model/PexelsImage.java
@@ -0,0 +1,63 @@
+package io.github.mazleo.snap.model;
+
+import android.graphics.Bitmap;
+
+public class PexelsImage extends PexelsElement {
+    private String imageUrl;
+    private int photographerId;
+    private String photographerName;
+    private Bitmap thumbnailBitmap;
+    private Bitmap srcBitmap;
+
+    public PexelsImage(int id, int width, int height, String imageUrl) {
+        super(id, width, height);
+        this.imageUrl = imageUrl;
+        this.photographerId = -1;
+        this.photographerName = null;
+    }
+    public PexelsImage(int id, int width, int height, String imageUrl, int photographerId, String photographerName) {
+        this(id, width, height, imageUrl)
+        this.photographerId = photographerId;
+        this.photographerName = photographerName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getPhotographerId() {
+        return photographerId;
+    }
+
+    public void setPhotographerId(int photographerId) {
+        this.photographerId = photographerId;
+    }
+
+    public String getPhotographerName() {
+        return photographerName;
+    }
+
+    public void setPhotographerName(String photographerName) {
+        this.photographerName = photographerName;
+    }
+
+    public Bitmap getThumbnailBitmap() {
+        return thumbnailBitmap;
+    }
+
+    public void setThumbnailBitmap(Bitmap thumbnailBitmap) {
+        this.thumbnailBitmap = thumbnailBitmap;
+    }
+
+    public Bitmap getSrcBitmap() {
+        return srcBitmap;
+    }
+
+    public void setSrcBitmap(Bitmap srcBitmap) {
+        this.srcBitmap = srcBitmap;
+    }
+}
