3f45a39ba8e88561ccb468eb783211d775c0c7ae
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-30 15:13:55 -0400

    Implement SearchState class

---
+++ app/src/main/java/io/github/mazleo/snap/model/SearchState.java
@@ -0,0 +1,55 @@
+package io.github.mazleo.snap.model;
+
+public class SearchState {
+    private int totalResults;
+    private int openPages;
+    private int resultsPerPage;
+    private int searchType;
+
+    public SearchState() {
+        this.totalResults = 0;
+        this.openPages = 0;
+        this.resultsPerPage = 0;
+        this.searchType = 0;
+    }
+    public SearchState(int totalResults) {
+        this();
+        this.totalResults = totalResults;
+    }
+    public SearchState(int totalResults, int openPages) {
+        this(totalResults);
+        this.openPages = openPages;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getOpenPages() {
+        return openPages;
+    }
+
+    public void setOpenPages(int openPages) {
+        this.openPages = openPages;
+    }
+
+    public int getResultsPerPage() {
+        return resultsPerPage;
+    }
+
+    public void setResultsPerPage(int resultsPerPage) {
+        this.resultsPerPage = resultsPerPage;
+    }
+
+    public int getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(int searchType) {
+        this.searchType = searchType;
+    }
+}
