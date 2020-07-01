4f712d109d0691523055182153a9421f384368f7
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-06-30 21:03:08 -0400

    Add API key

--- app/src/main/java/io/github/mazleo/snap/utils/SearchInfo.java
+++ app/src/main/java/io/github/mazleo/snap/utils/SearchInfo.java
@@ -6,4 +6,5 @@ public class SearchInfo {
     public static final int SEARCH_TYPE_VIDEO = 2;
     public static final int SEARCH_NO_PROGRESS = 0;
     public static final int SEARCH_IN_PROGRESS = 1;
+    public static final String PEXELS_API_KEY = "563492ad6f91700001000001da738e6f48fc43298205df5e5be3e92a";
 }
