da8dbab6c8984e243a2312b55bc7791b5012bc0e
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-07-08 13:01:29 -0400

    Implement parcelables

--- app/src/main/java/io/github/mazleo/snap/model/PexelsElement.java
+++ app/src/main/java/io/github/mazleo/snap/model/PexelsElement.java
@@ -1,8 +1,11 @@
 package io.github.mazleo.snap.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
-public class PexelsElement {
+public class PexelsElement implements Parcelable {
     private int id;
     private int width;
     private int height;
@@ -11,6 +14,36 @@ public class PexelsElement {
         this.id = id;
         this.width = width;
         this.height = height;
+    }
+
+    public PexelsElement(Parcel in) {
+        this.id = in.readInt();
+        this.width = in.readInt();
+        this.height = in.readInt();
+    }
+
+    public static final Parcelable.Creator<PexelsElement> CREATOR = new Parcelable.Creator<PexelsElement>() {
+        @Override
+        public PexelsElement createFromParcel(Parcel parcel) {
+            return new PexelsElement(parcel);
+        }
+
+        @Override
+        public PexelsElement[] newArray(int i) {
+            return new PexelsElement[i];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(this.id);
+        parcel.writeInt(this.width);
+        parcel.writeInt(this.height);
     }
 
     public int getId() {
--- app/src/main/java/io/github/mazleo/snap/model/PexelsImage.java
+++ app/src/main/java/io/github/mazleo/snap/model/PexelsImage.java
@@ -1,8 +1,10 @@
 package io.github.mazleo.snap.model;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class PexelsImage extends PexelsElement {
+public class PexelsImage extends PexelsElement implements Parcelable {
     private String imageUrl;
     private int photographerId;
     private String photographerName;
@@ -19,6 +21,30 @@ public class PexelsImage extends PexelsElement {
         this.photographerId = photographerId;
         this.photographerName = photographerName;
     }
+    public PexelsImage(Parcel in) {
+        super(in);
+        this.imageUrl = in.readString();
+        if (in.dataAvail() > 0) {
+            this.photographerId = in.readInt();
+            this.photographerName = in.readString();
+        }
+        else {
+            this.photographerId = -1;
+            this.photographerName = null;
+        }
+    }
+
+    public static final Parcelable.Creator<PexelsImage> CREATOR = new Parcelable.Creator<PexelsImage>() {
+        @Override
+        public PexelsImage createFromParcel(Parcel parcel) {
+            return new PexelsImage(parcel);
+        }
+
+        @Override
+        public PexelsImage[] newArray(int i) {
+            return new PexelsImage[i];
+        }
+    };
 
     public String getImageUrl() {
         return imageUrl;
@@ -50,5 +76,18 @@ public class PexelsImage extends PexelsElement {
 
     public void setThumbnailBitmap(Bitmap thumbnailBitmap) {
         this.thumbnailBitmap = thumbnailBitmap;
+    }
+
+    @Override
+    public int describeContents() {
+        return super.describeContents();
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        super.writeToParcel(parcel, i);
+        parcel.writeString(this.imageUrl);
+        parcel.writeInt(this.photographerId);
+        parcel.writeString(this.photographerName);
     }
 }
