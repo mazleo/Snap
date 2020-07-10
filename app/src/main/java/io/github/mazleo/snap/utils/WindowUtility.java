e23d231754aeabe4c5b07f2a21d21db1187de04c
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-07-10 10:19:22 -0400

    Clean up WindowUtility

--- app/src/main/java/io/github/mazleo/snap/utils/WindowUtility.java
+++ app/src/main/java/io/github/mazleo/snap/utils/WindowUtility.java
@@ -6,25 +6,31 @@ public class WindowUtility {
     public static int getWidthPX(Activity activity) {
         return activity.getWindowManager().getDefaultDisplay().getWidth();
     }
+
     public static int getHeightPX(Activity activity) {
         return activity.getWindowManager().getDefaultDisplay().getHeight();
     }
+
     public static float getWidthDP(Activity activity) {
         int widthPX = getWidthPX(activity);
         int dpi = DisplayUtility.getScreenDensityDPI(activity);
         return convertPXToDP(widthPX, dpi);
     }
+
     public static float getHeightDP(Activity activity) {
         int heightPX = getHeightPX(activity);
         int dpi = DisplayUtility.getScreenDensityDPI(activity);
         return convertPXToDP(heightPX, dpi);
     }
+
     public static float convertPXToDP(int px, int dpi) {
         return ((float) px / ((float) dpi / 160));
     }
+
     public static float convertDPToPX(int dp, int dpi) {
         return (int) ((float) dp * ((float) dpi / 160f));
     }
+
     public static float calcScreenAspectRatio(Activity activity) {
         float width = getWidthPX(activity);
         float height = getHeightPX(activity);
